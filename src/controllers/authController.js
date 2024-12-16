73ef0a903167d609ac8f55b2e358755bb6cfffa3
Manuel <manuelusero6@gmail.com>
2024-12-16 14:28:54 -0300

    ..

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -60,7 +60,7 @@ export const verifyUser = async (req, res) => {
 
     console.log("Cuenta verificada correctamente. Redirigiendo al login...");
 
-    res.redirect('https://whatsapp-clone-frontend-lilac.vercel.app/login');
+    // res.redirect('https://whatsapp-clone-frontend-lilac.vercel.app/login');
   } catch (error) {
     console.error('Error al verificar usuario:', error);
     res.status(500).json({ message: 'Error al verificar el usuario', error });
@@ -183,7 +183,7 @@ export const sendVerificationEmail = async (email, verificationToken) => {
     html: `
       <h2>¡Bienvenido a CloneWhatsapp!</h2>
       <p>Por favor, haz clic en el siguiente enlace para verificar tu cuenta:</p>
-      <a href="https://whatsapp-clone-frontend-lilac.vercel.app/login">Verificar cuenta</a>
+      <a href="https://whatsapp-clone-frontend-lilac.vercel.app/verify/${verificationToken}">Verificar cuenta</a>
     `,
   };
   await transporter.sendMail(mailOptions);
